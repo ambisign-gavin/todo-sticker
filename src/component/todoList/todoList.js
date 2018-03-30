5e1dbd2129429655707111b617e52f973d563b06
kuku8990 <kuku8990@gmail.com>
2018-03-30 15:14:40 +0800

    增加選擇編輯 todo 時會開啟編輯 modal 並顯示該 todo 資訊

--- src/component/todoList/todoList.js
+++ src/component/todoList/todoList.js
@@ -4,39 +4,80 @@ import { List, Icon } from 'antd';
 import type { EventState } from '../../states';
 import './todoList.scss';
 import moment from 'moment';
+import EditModal from '../eventEditModal/eventEditModal';
+import Translate from '../../class/translate';
 
 type Props = {
     todoLists: EventState[]
 }
 
-export default class TodoList extends React.Component<Props> {
+type States = {
+    visibleModal: boolean,
+    selectedId: number,
+    selectedNotificationDate: ?number,
+    selectedNotificationTime: ?number,
+    selectedDescription: string,
+}
+
+export default class TodoList extends React.Component<Props, States> {
+
+    handleEditTodo: Function;
+
+    state = {
+        visibleModal: false,
+        selectedId: 0,
+        selectedNotificationDate: null,
+        selectedNotificationTime: null,
+        selectedDescription: '',
+    }
 
     constructor(props: Props) {
         super(props);
+        this.handleEditTodo = this.handleEditTodo.bind(this);
+    }
+
+    handleEditTodo(todo: EventState) {
+        this.setState({
+            visibleModal: true,
+            selectedId: todo.id,
+            selectedDescription: todo.description,
+            selectedNotificationDate: todo.notificationDate,
+            selectedNotificationTime: todo.notificationTime,
+        });
     }
 
     render() {
         return (
-            <List
-                dataSource={this.props.todoLists}
-                renderItem={
-                    (item: EventState) => (
-                        <List.Item actions={[<a>edit</a>, <a>more</a>]} >
-                            <div className="list-row"  >
-                                <div className="clock">
-                                    <Icon type="clock-circle-o" />
+            <div>
+                <List
+                    dataSource={this.props.todoLists}
+                    renderItem={
+                        (item: EventState) => (
+                            <List.Item actions={[<a onClick={() => this.handleEditTodo(item)} >edit</a>, <a>more</a>]} >
+                                <div className="list-row"  >
+                                    <div className="clock">
+                                        <Icon type="clock-circle-o" />
+                                    </div>
+                                    <div className="notification-time">
+                                        {moment(item.notificationDate).format('YYYY/MM/DD')} - {moment(item.notificationTime).format('HH:mm')}
+                                    </div>
+                                    <div className="description">
+                                        {item.description}
+                                    </div>
                                 </div>
-                                <div className="notification-time">
-                                    {moment(item.notificationDate).format('YYYY/MM/DD')} - {moment(item.notificationTime).format('HH:mm')}
-                                </div>
-                                <div className="description">
-                                    {item.description}
-                                </div>
-                            </div>
-                        </List.Item>
-                    )
-                }
-            />
+                            </List.Item>
+                        )
+                    }
+                />
+                <EditModal
+                    title={Translate.tr('Edit Todo')}
+                    visible={this.state.visibleModal}
+                    onCancel={() => this.setState({visibleModal: false})}
+                    defaultDate={this.state.selectedNotificationDate || new Date().getTime()}
+                    defaultTime={this.state.selectedNotificationTime || new Date().getTime()}
+                    defaultDescriptin={this.state.selectedDescription}
+                />
+            </div>
         );
     }
 
