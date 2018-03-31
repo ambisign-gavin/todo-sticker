64cc9c57aa4a4b21c03af1d70b75e16946c0fe9c
kuku8990 <kuku8990@gmail.com>
2018-03-31 14:56:01 +0800

    調整 app 使用 grid 排版

--- src/component/app/app.js
+++ src/component/app/app.js
@@ -3,14 +3,16 @@ import React from 'react';
 import AddEventButton from '../addButton/addButton';
 import TodoListContainer from '../../container/todoListContainer';
 import './app.scss';
+import FilterButton from '../filterButton/FilterButton';
 
 export default class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="todo-container" >
                 App
-                <AddEventButton className="float-right-btn" />
+                <AddEventButton className="add-button" />
+                <FilterButton className="filter-button" />
                 <div className="lists-area">
                     <TodoListContainer />
                 </div>
