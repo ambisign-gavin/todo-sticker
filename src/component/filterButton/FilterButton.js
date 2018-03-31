5a72c2a09c1e2cad23cc2b11329a4d189169cd01
kuku8990 <kuku8990@gmail.com>
2018-03-31 16:40:08 +0800

    增加點擊 filter 按鈕開啟 filter panel 功能

--- src/component/filterButton/FilterButton.js
+++ src/component/filterButton/FilterButton.js
@@ -2,11 +2,32 @@
 import React from 'react';
 import { Button } from 'antd';
 import Translate from '../../class/translate';
+import FilterPanel from '../filterPanel/FilterPanel';
 
 type Props = {
 
 }
 
-export default (props: Props) => (
-    <Button {...props}>{Translate.tr('filter')}</Button>
-);
+type States = {
+    showPanel: boolean,
+}
+
+export default class FilterButton extends React.Component<Props, States> {
+
+    state: States = {
+        showPanel: false,
+    }
+
+    constructor(props: Props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div {...this.props}>
+                <Button onClick={() => this.setState({showPanel: !this.state.showPanel})} >{Translate.tr('filter')}</Button>
+                <FilterPanel show={this.state.showPanel} />
+            </div>
+        );
+    }
+}
