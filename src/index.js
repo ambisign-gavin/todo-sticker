8e51f2169790c076d9672a77c423f073191d1ff2
kuku8990 <kuku8990@gmail.com>
2018-04-05 20:55:13 +0800

    增加啟動程式時初始化通知列表功能

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+// @flow
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TodosPage from './component/todosPage/todosPage';
@@ -6,13 +8,21 @@ import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import '../node_modules/antd/dist/antd.min.css';
 import '../node_modules/antd/dist/antd.min.js';
+import type {TodoState} from './states/index';
+import NotifyServer from './class/notify/notifyServer';
+
 // localStorage.removeItem('todos');
 const initStorageData = localStorage.getItem('todos') || '{}';
-
 const store = createStore(rootReducer, JSON.parse(initStorageData));
 store.subscribe(() => {
     localStorage.setItem('todos', JSON.stringify(store.getState()));
     console.log(store.getState());
+});
+
+let notifyServer: NotifyServer = new NotifyServer();
+
+store.getState().todos.map((todo: TodoState) => {
+    notifyServer.addSchedule(todo.id || 'empty', todo.dueDatetime, todo.description);
 });
 
 ReactDOM.render(
