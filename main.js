f03b946fb09f5d84d7b268c6052075b9119746c6
kuku8990 <kuku8990@gmail.com>
2018-04-07 16:37:29 +0800

    修改關閉應用程式時 dock 仍保留，且點選 dock 會開啟列表頁面

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ const url = require('url');
 let win;
 
 declare var __dirname: any;
+declare var process: any;
 
 function createWindow() {
     // 创建浏览器窗口。
@@ -43,21 +44,21 @@ let icHandler = new IpcHandler();
 icHandler.registerListener();
 
 // 当全部窗口关闭时退出。
-// app.on('window-all-closed', () => {
-//     // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
-//     // 否则绝大部分应用及其菜单栏会保持激活。
-//     if (process.platform !== 'darwin') {
-//         app.quit()
-//     }
-// })
+app.on('window-all-closed', () => {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定地退出，
+    // 否则绝大部分应用及其菜单栏会保持激活。
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
-// app.on('activate', () => {
-//     // 在macOS上，当单击dock图标并且没有其他窗口打开时，
-//     // 通常在应用程序中重新创建一个窗口。
-//     if (win === null) {
-//         createWindow()
-//     }
-// })
+app.on('activate', () => {
+    // 在macOS上，当单击dock图标并且没有其他窗口打开时，
+    // 通常在应用程序中重新创建一个窗口。
+    if (win === null) {
+        createWindow();
+    }
+});
 
 // 在这文件，你可以续写应用剩下主进程代码。
 // 也可以拆分成几个文件，然后用 require 导入。
