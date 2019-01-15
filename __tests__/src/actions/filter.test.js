05d2a6f2ea9554f09abc4a25597ccdac1fae5c46
kuku8990 <kuku8990@gmail.com>
2019-01-15 23:25:45 +0800

    Make action tests not use redux store
    Make action tests not use the redux store, only test action creator return object, and make sure it's correct.

--- __tests__/src/actions/filter.test.js
+++ __tests__/src/actions/filter.test.js
@@ -1,55 +1,39 @@
 // @flow
-import { createStore, type Store } from 'redux';
 import { settingDueDateFilter, settingCompleteStatusFilter } from '../../../src/actions/filter';
 import { DueDateFilterEnum, CompleteStatusFilterEnum } from '../../../src/constant/filter';
 
 describe('filter action', () => {
-    let store: Store;
-    let reducer = jest.fn();
-    beforeAll(() => {
-        store = createStore(reducer);
-    });
 
     it('should use date filter of all option', () => {
-        store.dispatch(settingDueDateFilter(DueDateFilterEnum.all));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingDueDateFilter(DueDateFilterEnum.all)).toEqual({
             type: 'dueDateFilter',
             filter: 'all',
         });
     });
 
     it('should use date filter of today option', () => {
-        store.dispatch(settingDueDateFilter(DueDateFilterEnum.today));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingDueDateFilter(DueDateFilterEnum.today)).toEqual({
             type: 'dueDateFilter',
             filter: 'today',
         });
     });
 
     it('should use complete status filter of all option', () => {
-        store.dispatch(settingCompleteStatusFilter(CompleteStatusFilterEnum.all));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingCompleteStatusFilter(CompleteStatusFilterEnum.all)).toEqual({
             type: 'completeStatusFilter',
             filter: 'all',
         });
     });
 
     it('should use complete status filter of complete option', () => {
-        store.dispatch(settingCompleteStatusFilter(CompleteStatusFilterEnum.complete));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingCompleteStatusFilter(CompleteStatusFilterEnum.complete)).toEqual({
             type: 'completeStatusFilter',
             filter: 'complete',
         });
     });
 
     it('should use complete status filter of uncomplete option', () => {
-        store.dispatch(settingCompleteStatusFilter(CompleteStatusFilterEnum.uncomplete));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingCompleteStatusFilter(CompleteStatusFilterEnum.uncomplete)).toEqual({
             type: 'completeStatusFilter',
             filter: 'uncomplete',
         });
--- __tests__/src/actions/sort.test.js
+++ __tests__/src/actions/sort.test.js
@@ -1,56 +1,39 @@
 // @flow
-import { createStore, type Store } from 'redux';
 import { settingSortColumn, settingSortBy } from '../../../src/actions/sort';
 import { SortColumnEnum, SortByTypeEnum } from '../../../src/constant/sort';
 
 describe('sort action', () => {
-    let store: Store;
-    let reducer = jest.fn();
-
-    beforeAll(() => {
-        store = createStore(reducer);
-    });
 
     it('should change the column of sort feature to createTime', () => {
-        store.dispatch(settingSortColumn(SortColumnEnum.createTime));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingSortColumn(SortColumnEnum.createTime)).toEqual({
             type: 'sortColumn',
             column: 'createTime',
         });
     });
 
     it('should change the column of sort feature to dueDate', () => {
-        store.dispatch(settingSortColumn(SortColumnEnum.dueDate));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingSortColumn(SortColumnEnum.dueDate)).toEqual({
             type: 'sortColumn',
             column: 'dueDate',
         });
     });
 
     it('should change the column of sort feature to createTime', () => {
-        store.dispatch(settingSortColumn(SortColumnEnum.createTime));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingSortColumn(SortColumnEnum.createTime)).toEqual({
             type: 'sortColumn',
             column: 'createTime',
         });
     });
 
     it('should change the rule of sort feature to desc', () => {
-        store.dispatch(settingSortBy(SortByTypeEnum.desc));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingSortBy(SortByTypeEnum.desc)).toEqual({
             type: 'sortBy',
             sortBy: 'desc',
         });
     });
 
     it('should change the rule of sort feature to asc', () => {
-        store.dispatch(settingSortBy(SortByTypeEnum.asc));
-        expect(reducer.mock.calls.length).toEqual(1);
-        expect(reducer.mock.calls[0][1]).toEqual({
+        expect(settingSortBy(SortByTypeEnum.asc)).toEqual({
             type: 'sortBy',
             sortBy: 'asc',
         });
