c7ba1d579c48b93189817e7893b4202ec8ba4d19
kuku8990 <kuku8990@gmail.com>
2019-01-16 15:37:05 +0800

    Change todosPage use styled-components

--- src/component/todosPage/todosPage.js
+++ src/component/todosPage/todosPage.js
@@ -3,10 +3,10 @@
 import React from 'react';
 import AddEventButton from '../addButton/addButton';
 import TodoListContainer from '../../container/todoListContainer';
-import './todosPage.scss';
 import FilterButton from '../filterButton/FilterButton';
 import FilterPanelContainer from '../../container/FilterPanelContainer';
 import SortButtonContainer from '../../container/sortButtonContainer';
+import styled from 'styled-components';
 
 type Props = {
 
@@ -37,26 +37,83 @@ export default class TodosPage extends React.Component<Props, States> {
 
     render() {
         let toolBar = (
-            <div className="tool-bar"><AddEventButton className="add-btn" /></div>
+            <ToolBar><AddEventToolButton/></ToolBar>
         );
 
         return (
-            <div className="todo-container" >
+            <TodosContainer>
                 {toolBar}
-                <div className="todos-view" >
-                    <div className="filter-button">
+                <TodosView>
+                    <FilterButtonDiv>
                         <FilterButton onClick={() => this.setState({showPanel: !this.state.showPanel})}  />
                         <FilterPanelContainer onHidden={this.handlePanelHidden} show={this.state.showPanel} />
-                    </div>
-                    <div className="lists-area">
+                    </FilterButtonDiv>
+                    <TodoListDiv>
                         <TodoListContainer />
-                    </div>
+                    </TodoListDiv>
 
-                    <div className="sort-btn">
-                        <SortButtonContainer style={{display: 'inline'}}/>
-                    </div>
-                </div>
-            </div>
+                    <SortButtonDiv style={{display: 'inline'}}/>
+                </TodosView>
+            </TodosContainer>
         );
     }
 }
+
+const TodosContainer = styled.div`
+    width: 100%;
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 10% 70% 20%;
+    grid-template-rows: 10% 90%;
+    grid-row-gap: 5%;
+    grid-template-areas:
+    "tool-bar tool-bar tool-bar"
+    "todos-view todos-view todos-view";
+    background-color: #d8d8d8;
+`;
+
+const AddEventToolButton = styled(AddEventButton)`
+    align-self: center;
+    margin-right: 10px;
+`;
+
+const ToolBar = styled.div`
+    grid-area: tool-bar;
+    display: flex;
+    justify-content: flex-end;
+    background-color: #fcfcfc;
+    box-shadow: 0px 5px 15px #a5a5a5;
+`;
+
+const TodosView = styled.div`
+    grid-area: todos-view;
+    display: grid;
+    width: 95%;
+    height: 90%;
+    justify-self: center;
+    align-self: start;
+    grid-row-gap: 10px;
+    grid-template-columns: 10% 70% 20%;
+    grid-template-rows: 10% 90%;
+    grid-template-areas:
+    "filterButton sort-btn sort-btn"
+    "listArea listArea listArea";
+    background-color: #fcfcfc;
+`;
+
+const FilterButtonDiv = styled.div`
+    grid-area: filterButton;
+    align-self: end;
+    margin-left: 10px;
+`;
+
+const TodoListDiv = styled.div`
+    grid-area: listArea;
+`;
+
+const SortButtonDiv = styled(SortButtonContainer)`
+    grid-area: sort-btn;
+    justify-self: end;
+    align-self: end;
+    margin-right: 10px;
+`;
