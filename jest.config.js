8126c8dfcc0133f0bb72b62901dcac9be434e4b2
kuku8990 <kuku8990@gmail.com>
2019-01-16 17:38:48 +0800

    Add jest-styled-components module and settings, and downgraded styled-components
    The jest-styled-components not support styled-components v4 yet, so need downgraded.

--- jest.config.js
+++ jest.config.js
@@ -120,10 +120,12 @@ module.exports = {
     setupFiles: ['<rootDir>/test-setup.js'],
 
     // The path to a module that runs some code to configure or set up the testing framework before each test
-    // setupTestFrameworkScriptFile: null,
+    setupTestFrameworkScriptFile: '<rootDir>/test-framework.js',
 
     // A list of paths to snapshot serializer modules Jest should use for snapshot testing
-    // snapshotSerializers: [],
+    snapshotSerializers: [
+        'enzyme-to-json/serializer'
+    ],
 
     // The test environment that will be used for testing
     // testEnvironment: "jest-environment-jsdom",
