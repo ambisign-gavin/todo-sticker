c8af654aeff86957b4d45816a56b3780b5331d0a
kuku8990 <kuku8990@gmail.com>
2019-03-06 16:16:05 +0800

    Remove the electron files

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
-import Hello from './component/hello';
-
-
-ReactDOM.render(
-    <Hello name="Gavin" />,
-    document.getElementById("app")
-)
